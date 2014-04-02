36ec5fd758b04ba81403f07ce67614c38a8cbd3c
Daeyun Shin <daeyunshin@gmail.com>
2014-04-01 23:16:45 -0500

    Clean up demos.

--- demos/multiple.m
+++ demos/multiple.m
@@ -1,7 +1,7 @@
 images = cell(1, 3);
-images{1} = imread('image_stitching/img/assignment3_data/ledge/1.JPG');
-images{2} = imread('image_stitching/img/assignment3_data/ledge/2.JPG');
-images{3} = imread('image_stitching/img/assignment3_data/ledge/3.JPG');
+images{1} = imread('image_stitching/img/ledge/1.JPG');
+images{2} = imread('image_stitching/img/ledge/2.JPG');
+images{3} = imread('image_stitching/img/ledge/3.JPG');
 
 [result, H, num_inliers, residual] = ...
     stitch_images(images, 5, 5, 0.03, 1, 100, 4000);
--- demos/transform.m
+++ demos/transform.m
@@ -2,25 +2,25 @@
 % http://www.leet.it/home/giusti/teaching/matlab_sessions/stitching/stitch.html
 
 x1 = 1.0e+03 * [0.8860;
-    1.5505;
-    1.5835;
-    1.3225];
+     1.5505;
+     1.5835;
+     1.3225];
 y1 = [371.7500;
-  386.7500;
-  833.7500;
-  881.7500];
+      386.7500;
+      833.7500;
+      881.7500];
 x2 = [41.5000;
-  697.0000;
-  724.0000;
-  482.5000];
-y2 = 1.0e+03 *[0.5563;
-    0.5668;
-    0.9823;
-    1.0588];
+      697.0000;
+      724.0000;
+      482.5000];
+y2 = 1.0e+03 * [0.5563;
+     0.5668;
+     0.9823;
+     1.0588];
 
-T = maketform('projective',[x2 y2],[x1 y1]);
-H=(T.tdata.T)'
-H=fit_homography(x2, y2, x1, y1)
+T = maketform('projective', [x2 y2], [x1 y1]);
+H=(T.tdata.T)';
+H=fit_homography(x2, y2, x1, y1);
 
 X = [x2 y2 ones(size(y1,1),1)]';
 
